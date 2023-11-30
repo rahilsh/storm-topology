a3321eba2b708072cadfc32eba9a5457887d1853
Rahil S <rahilrshk@gmail.com>
2023-11-30 16:07:40 +0530

    Upgraded to java 17

--- topology/src/main/java/in/rsh/st/topology/module/AppModule.java
+++ topology/src/main/java/in/rsh/st/topology/module/AppModule.java
@@ -1,22 +1,16 @@
 package in.rsh.st.topology.module;
 
-import com.google.inject.Binder;
-import com.google.inject.Module;
+import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.name.Names;
 import in.rsh.st.topology.client.DummyClass;
-import javax.inject.Named;
 
-public class AppModule implements Module {
+public class AppModule extends AbstractModule {
 
   @Override
-  public void configure(Binder binder) {}
-
-  @Provides
-  @Singleton
-  @Named("dummyConstant")
-  public String getDummyConstant() {
-    return "dummyValue";
+  protected void configure() {
+    bindConstant().annotatedWith(Names.named("dummyConstant")).to("Hello, Guice!");
   }
 
   @Provides
